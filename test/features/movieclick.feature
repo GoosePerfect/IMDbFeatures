13b8732dab0deb4fa79e8776f9ad7d65e2cb665f
anneliey <annelieyan@gmail.com>
2021-12-06 14:03:43 +0100

    testing finished

--- test/features/movieclick.feature
+++ test/features/movieclick.feature
@@ -1,3 +1,17 @@
 Feature: To test the movie page functionality 
+    "As a user I want to search movies, that want to find."
 
     Scenario: Play trailer    
+        Given I am on the movie page
+        When I click the button "Play"
+        Then I see results containing the movie trailer in redirected page
+
+    Scenario: Actor info page
+        Given I am on the movie page
+        When I click on the actor
+        Then I get redirected to actors info page
+
+    Scenario: User review page
+        Given I am on the movie page
+        When I click on User reviews
+        Then I get redirected to user reviews
--- test/step-definitions/movieclick.steps.js
+++ test/step-definitions/movieclick.steps.js
@@ -1,13 +1,37 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
-
+// Scenario 1
 Given('I am on the movie page', async () => {
     await browser.url(`https://www.imdb.com/title/tt1375666/?ref_=nv_sr_srsg_6`);
 });
 
-When('', async () => {
-
+When('I click the button "Play"', async () => {   
+    await $('*[href*="/video/vi2959588889?playlistId=tt1375666"]').click();    
 });
 
-Then('', async () => {
-
+ Then('I see results containing the movie trailer in redirected page', async () => {
+    await expect(browser).toHaveUrlContaining('/video/vi2959588889?playlistId=tt1375666')
 }); 
+
+
+ //Scenario 2
+When('I click on the actor', async () => {
+    await $('*[href="/name/nm0000138/?ref_=tt_ov_st"]').click(); 
+});
+
+
+Then('I get redirected to actors info page', async () => {
+    await expect(browser).toHaveUrlContaining('/name/nm0000138/?ref_=tt_ov_st')
+});
+
+
+// Scenario 3
+When('I click on User reviews', async () => {
+    await $('*[href="/title/tt1375666/reviews?ref_=tt_ov_rt"]').click(); 
+});
+
+Then('I get redirected to user reviews', async () => {
+    await expect(browser).toHaveUrlContaining('/title/tt1375666/reviews?ref_=tt_ov_rt')
+});
+
+
+
