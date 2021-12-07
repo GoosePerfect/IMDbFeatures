2aa40ce137c6a7f24a93bc6d9c23c3daf1765d5d
My Grandin <my.grandin@gmail.com>
2021-12-07 11:25:05 +0100

    variable changes

--- test/step-definitions/popularmovie.steps.js
+++ test/step-definitions/popularmovie.steps.js
@@ -3,16 +3,16 @@ const { Given, When, Then } = require('@wdio/cucumber-framework');
 //////////////// Scenario 1 /////////////////////
 
 Given('I am on IMDbs home page', async () =>{
-    await browser.url('https://www.imdb.com/');
+    await browser.url('https://www.imdb.com');
 });
 
 When('I click on menu button', async () => {
-    let clickOnMenuButton = await($('*[class="ipc-icon ipc-icon--menu ipc-button__icon ipc-button__icon--pre"]'));
+    const clickOnMenuButton = await($('*[class="ipc-icon ipc-icon--menu ipc-button__icon ipc-button__icon--pre"]'));
     clickOnMenuButton.click();
 });
 
 When('I click on Most Popular Movie', async () => {
-    let clickOnMostPopularMovie = await($('[href="/chart/moviemeter/?ref_=nv_mv_mpm"]'));
+    const clickOnMostPopularMovie = await($('[href="/chart/moviemeter/?ref_=nv_mv_mpm"]'));
     clickOnMostPopularMovie.click();
 });
 
@@ -28,12 +28,12 @@ Given('I am on the IMDbs Most Popular Movie Page', async () => {
 });
 
 When('I click on the sort by dropdown menu', async () => {
-    let clickOnDropDownMenu = await ($('*[class=lister-sort-by]'));
+    const clickOnDropDownMenu = await ($('*[class=lister-sort-by]'));
     clickOnDropDownMenu.click();
 });
 
 When('I select IMDbs rating', async () => {
-    let clickOnIMDbsRating = await ($('*[value="ir:descending"]'));
+    const clickOnIMDbsRating = await ($('*[value="ir:descending"]'));
     clickOnIMDbsRating.click();
 });
 
@@ -44,7 +44,7 @@ Then('I see most popular movies sorted by IMDbs rating', async() => {
 //////////////// Scenario 3 /////////////////////
 
 When('I select Release date', async () => {
-    let clickOnIMDbsRating = await ($('*[value="us:descending"]'));
+    const clickOnIMDbsRating = await ($('*[value="us:descending"]'));
     clickOnIMDbsRating.click();
 });
 
@@ -59,7 +59,7 @@ Given('I see movies sorted by release date', async () => {
 });
 
 When('I click on descending order', async () => {
-    let clickOnDescendingOrder = await ($('*[data-sort="rk:desc"]'));
+    const clickOnDescendingOrder = await ($('*[data-sort="rk:desc"]'));
     clickOnDescendingOrder.click();
 });
 
